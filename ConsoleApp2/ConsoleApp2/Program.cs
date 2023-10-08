393e4a636194bf0f0d11ba834b55cd57b2924e69
Orxan <ibrahimzada003@gmail.com>
2023-10-08 14:52:59 +0400

    c#

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Channels;
+
+int num1 = 5;
+int num2 = 4;
+num1 = num1 + num2;
+num2 = num1 - num2;
+num1 = num1 - num2;
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
